2a82d037fef6f2d4cafb419f89fc843251169037
Brian Kim <gittybrian@gmail.com>
2022-03-25 10:44:37 -0400

    added: lottie animation

--- client/src/pages/landing/Components/BlockA/index.tsx
+++ client/src/pages/landing/Components/BlockA/index.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import Lottie from 'lottie-web';
 
 import styles from './styles.module.scss';
 import steak from '../../../../assets/steak.png';
@@ -21,6 +22,18 @@ const BlockA = () => {
 	// 	setInterval(transitionImage, 1000);
 	// }, [currentImage]);
 
+	const animation = useRef(null);
+
+	useEffect(() => {
+		Lottie.loadAnimation({
+			container: animation.current!,
+			renderer: 'svg',
+			loop: true,
+			autoplay: true,
+			animationData: require('../../../../assets/course.json')
+		});
+	}, [animation]);
+
 	return (
 		<div className={styles.container}>
 			<div className={styles['component-container']}>
@@ -29,10 +42,10 @@ const BlockA = () => {
 					<h1>Teach.</h1>
 					<h1>Share.</h1>
 				</div>
-				<div className={styles['right-component']}>
-					<img src={blocka} alt="steak" height="650" />
+				<div className={styles['right-component']} ref={animation}>
+					{/* <img src={blocka} alt="steak" height="650" />
 					<img className={styles['image-component']} id="image" src={currentImage} alt="steak" height="100" />
-					<TypeComponent />
+					<TypeComponent /> */}
 				</div>
 			</div>
 		</div>
