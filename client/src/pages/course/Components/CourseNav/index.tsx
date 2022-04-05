a71cb19ca178c71b5c5fed5ed277e474a5276bc6
Brian Kim <gittybrian@gmail.com>
2022-04-04 23:16:29 -0400

    styling: pre-wrap spacing for content to html

--- client/src/pages/course/Components/CourseNav/index.tsx
+++ client/src/pages/course/Components/CourseNav/index.tsx
@@ -1,9 +1,12 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import config from '../../../../config/config';
 import logging from '../../../../config/logging';
+// import ILesson from '../../../interfaces/lesson';
+
+import ICourse from '../../../../interfaces/course';
 
 import styles from './styles.module.scss';
 
@@ -16,19 +19,48 @@ export interface IProps {
 	lessons: ILessons[];
 }
 
-const CourseNav: React.FunctionComponent<IProps> = (props) => {
+const EditNav: React.FunctionComponent<IProps> = (props) => {
+	const [course, setCourse] = useState<ICourse>();
 	const { lessons } = props;
-	const { courseID } = useParams();
+	const { courseID, lessonID } = useParams();
+	const navigate = useNavigate();
+
+	const getCourse = async () => {
+		try {
+			const response = await axios.get(`${config.server.url}/course/${courseID}`);
+
+			if (response.status === 200) {
+				setCourse(response.data.course);
+			} else {
+				logging.error('Unable to get course');
+			}
+		} catch (error) {
+			logging.error(error);
+		}
+	};
+
+	useEffect(() => {
+		getCourse();
+	}, []);
 
 	return (
-		<nav>
-			<h1>Nav for Course</h1>
+		<nav className={styles['navbar']}>
+			<Link to={`/course/${courseID}`}>
+				<p className={styles['title']}>{course && course.title}</p>
+			</Link>
 			<ul>
 				{lessons &&
 					lessons.map((ele, idx) => {
 						return (
 							<li key={idx}>
-								<Link to={`/course/${courseID}/${ele.lessonId}`}>{ele.lessonTitle}</Link>
+								<Link
+									className={
+										ele.lessonId === lessonID ? `${styles.link} ${styles.selected}` : styles['link']
+									}
+									to={`/course/${courseID}/${ele.lessonId}`}
+								>
+									{ele.lessonTitle}
+								</Link>
 							</li>
 						);
 					})}
@@ -37,4 +69,4 @@ const CourseNav: React.FunctionComponent<IProps> = (props) => {
 	);
 };
 
-export default CourseNav;
+export default EditNav;
--- client/src/pages/course/index.tsx
+++ client/src/pages/course/index.tsx
@@ -22,7 +22,7 @@ const CoursePage: React.FunctionComponent<any> = (props) => {
 	const [_id, setId] = useState<string>('');
 	const [course, setCourse] = useState<ICourse | null>(null);
 	const [navLessons, setNavLessons] = useState<ILessons[]>([]);
-	const [lesson, setLesson] = useState<ILesson>();
+	const [lesson, setLesson] = useState<ILesson | null>();
 	const [error, setError] = useState<string>('');
 
 	const [state, setState] = useContext(UserContext);
@@ -39,6 +39,7 @@ const CoursePage: React.FunctionComponent<any> = (props) => {
 				logging.error('Unable to set course');
 			}
 		} catch (error) {
+			console.log('catch entered');
 			logging.error(error);
 			navigate('/error');
 		}
@@ -59,16 +60,18 @@ const CoursePage: React.FunctionComponent<any> = (props) => {
 	};
 
 	const getLesson = async () => {
-		try {
-			const response = await axios.get(`${config.server.url}/lesson/${lessonID}`);
+		if (lessonID) {
+			try {
+				const response = await axios.get(`${config.server.url}/lesson/${lessonID}`);
 
-			if (response.status === 200) {
-				setLesson(response.data.lesson);
-			} else {
-				logging.error('Unable to find lesson');
+				if (response.status === 200) {
+					setLesson(response.data.lesson);
+				} else {
+					logging.error('Unable to find lesson');
+				}
+			} catch (error) {
+				logging.error(error);
 			}
-		} catch (error) {
-			logging.error(error);
 		}
 	};
 
@@ -79,25 +82,33 @@ const CoursePage: React.FunctionComponent<any> = (props) => {
 
 	useEffect(() => {
 		getLesson();
+		if (!lessonID) {
+			setLesson(null);
+		}
 	}, [lessonID]);
 
 	if (course) {
-		if (lesson) {
-			return (
-				<div>
-					<CourseNav lessons={navLessons} />
-					<div dangerouslySetInnerHTML={{ __html: lesson.content }} />
+		return (
+			<div>
+				<NavBar />
+				<CourseNav lessons={navLessons} />
+				<div className={styles['content-container']}>
+					{lesson ? (
+						<div>
+							<div dangerouslySetInnerHTML={{ __html: lesson.content }} />
+						</div>
+					) : (
+						<div>
+							<div>{course.title}</div>
+							<div
+								className={styles['html-content']}
+								dangerouslySetInnerHTML={{ __html: course.description }}
+							/>
+						</div>
+					)}
 				</div>
-			);
-		} else {
-			return (
-				<div>
-					<CourseNav lessons={navLessons} />
-					<div>{course.title}</div>
-					<div>{course.description}</div>
-				</div>
-			);
-		}
+			</div>
+		);
 	} else {
 		return <Link to="/home" />;
 	}
