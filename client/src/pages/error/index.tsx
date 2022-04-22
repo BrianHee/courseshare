7fc189cea1951675692c3195d99e7c6b3957c988
Brian Kim <gittybrian@gmail.com>
2022-04-22 11:58:55 -0400

    styling: error page

--- client/src/pages/error/index.tsx
+++ client/src/pages/error/index.tsx
@@ -6,8 +6,13 @@ import styles from './styles.module.scss';
 const ErrorPage: React.FunctionComponent = () => {
 	return (
 		<div className={styles.container}>
-			<p>Error</p>
-			<Link to="/home">Return Home</Link>
+			<div className={styles.wrapper}>
+				<h1>Whoops!</h1>
+				<h2>Looks like you are in uncharted territory ☠️...</h2>
+				<Link className={styles.link} to="/home">
+					Return Home
+				</Link>
+			</div>
 		</div>
 	);
 };
