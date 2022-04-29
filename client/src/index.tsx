45dc45af54b845d41a3859e17fe35d2942ffbc33
Brian Kim <gittybrian@gmail.com>
2022-04-29 10:57:23 -0400

    client: hashrouter for production

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 import App from './App';
 import { UserProvider } from './context';
@@ -9,9 +9,11 @@ import './index.scss';
 
 ReactDOM.render(
 	<UserProvider>
-		<BrowserRouter>
+		{/* <BrowserRouter> */}
+		<HashRouter>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
+		{/* </BrowserRouter> */}
 	</UserProvider>,
 	document.getElementById('root')
 );
