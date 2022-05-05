e44d9a73645238742ea62bb641408c9a17f69b6a
Brian Kim <gittybrian@gmail.com>
2022-05-04 23:11:02 -0400

    testing: coursepreview component

--- client/src/components/CoursePreview/coursePreview.test.tsx
+++ client/src/components/CoursePreview/coursePreview.test.tsx
@@ -1,22 +1,30 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
 import CoursePreview from './index';
 
 describe('CoursePreview Test', () => {
+	const testProps = {
+		_id: '1',
+		title: 'How to Prepare a New York Strip',
+		image: 'https://theviewfromgreatisland.com/wp-content/uploads/2020/01/new-york-strip-steak-sliced-scaled.jpg'
+	};
+
+	const testElement = (
+		<MemoryRouter>
+			<CoursePreview {...testProps} />
+		</MemoryRouter>
+	);
+
+	it('renders the course title', () => {
+		render(testElement);
+		const previewElement = screen.getByText(`${testProps.title}`);
+		expect(previewElement).toBeInTheDocument();
+	});
+
 	it('links to the course edit page', () => {
-		const testProps = {
-			_id: '1',
-			title: 'How to Prepare a New York Strip',
-			image: 'https://theviewfromgreatisland.com/wp-content/uploads/2020/01/new-york-strip-steak-sliced-scaled.jpg'
-		};
-		const { getByRole, queryByRole, queryByText } = render(
-			<MemoryRouter>
-				<CoursePreview {...testProps} />
-			</MemoryRouter>
-		);
-		// expect(getByRole('link')).toHaveAttribute('href', `/edit/${testProps._id}`);
-		// expect(getByRole('link')).toHaveTextContent(`${testProps.title}`);
-		expect(queryByText(`${testProps.title}`)).toBeFalsy();
+		render(testElement);
+		const previewElement = screen.getByText(`${testProps.title}`);
+		expect(previewElement).toBeInTheDocument();
 	});
 });
