8316d3ae5729642c02f1ed15e830b739a7f46d19
Brian Kim <gittybrian@gmail.com>
2024-02-10 10:23:29 -0500

    added: CORS security for server accepting  client url

--- server/src/index.ts
+++ server/src/index.ts
@@ -58,7 +58,7 @@ app.get('/', (req, res) => res.send('courseshare'));
 
 // API Access
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
 	if (req.method == 'OPTIONS') {
